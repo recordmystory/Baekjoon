fe079b532819e0ac2751c3f4af38d479678fabd2
_recordmystory <sdl13862@naver.com>
2023-06-22 21:35:43 +0900

    곱셈까지

---
+++ src/Joonas.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Joonas {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        String name=sc.next();
+
+
+        System.out.println(name+"??!");
+    }
+
+}
